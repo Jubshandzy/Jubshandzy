0f7e64325b08c452a4327ce6073c3b2ec3edda54
jubril <jubreal30@gmail.com>
2022-07-21 15:24:28 -0500

    new update 1

--- 2ndtestpush.sh
+++ 2ndtestpush.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "this is a sample to show an understanding of push to github"
-#didodogjdgigjdgp;d
+#didodogjdgigjdgp;heds
+skshbslgns.kgnm
+sgksjbnlgsn,sgns,gns.gs
+sgsbg,sngs,gbk,bgs ,gs
+shmg,bsg,sgn.sngs,ngsgns:g
