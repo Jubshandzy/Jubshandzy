48c5359cc23803322c282689012e289bc82b8dbc
Jubshandzy <96334396+Jubshandzy@users.noreply.github.com>
2022-07-21 21:33:52 +0100

    Update 2ndtestpush.sh

--- 2ndtestpush.sh
+++ 2ndtestpush.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 echo "this is a sample to show an understanding of push to github"
-#didodogjdgigjdgp;heds
-skshbslgns.kgnm
-sgksjbnlgsn,sgns,gns.gs
-sgsbg,sngs,gbk,bgs ,gs
-shmg,bsg,sgn.sngs,ngsgns:g
-gahoughergolnlgwk
-ljrhls.gbsrnbw.srklgnw
-erlgjbw.egjb.kerjbglwejrgk
+echo "practicing merge conflict and rebase"
+#this is by programmer 1
